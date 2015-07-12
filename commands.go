9da9ac74a8ad14812069b3a636806ddf79ef101a
Jiro Nishiguchi <jiro@cpan.org>
2015-07-12 21:43:34 +0900

    Indent

--- commands.go
+++ commands.go
@@ -18,28 +18,48 @@ func init() {
 	}
 	Commands = map[string]cli.CommandFactory{
 		"list": func() (cli.Command, error) {
-			return &command.ListCommand{UI: ui}, nil
+			return &command.ListCommand{
+				UI: ui,
+			}, nil
 		},
 		"cat": func() (cli.Command, error) {
-			return &command.CatCommand{UI: ui, Output: os.Stdout}, nil
+			return &command.CatCommand{
+				UI:     ui,
+				Output: os.Stdout,
+			}, nil
 		},
 		"put": func() (cli.Command, error) {
-			return &command.PutCommand{UI: ui, Input: os.Stdin}, nil
+			return &command.PutCommand{
+				UI:    ui,
+				Input: os.Stdin,
+			}, nil
 		},
 		"edit": func() (cli.Command, error) {
-			return &command.EditCommand{UI: ui}, nil
+			return &command.EditCommand{
+				UI: ui,
+			}, nil
 		},
 		"delete": func() (cli.Command, error) {
-			return &command.DeleteCommand{UI: ui}, nil
+			return &command.DeleteCommand{
+				UI: ui,
+			}, nil
 		},
 		"flags": func() (cli.Command, error) {
-			return &command.FlagsCommand{UI: ui}, nil
+			return &command.FlagsCommand{
+				UI: ui,
+			}, nil
 		},
 		"dump": func() (cli.Command, error) {
-			return &command.DumpCommand{UI: ui, Output: os.Stdout}, nil
+			return &command.DumpCommand{
+				UI:     ui,
+				Output: os.Stdout,
+			}, nil
 		},
 		"load": func() (cli.Command, error) {
-			return &command.LoadCommand{UI: ui, Input: os.Stdin}, nil
+			return &command.LoadCommand{
+				UI:    ui,
+				Input: os.Stdin,
+			}, nil
 		},
 	}
 }
