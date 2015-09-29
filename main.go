2d42de43f90fdb213a11f89f456c0762f3c983db
Jiro Nishiguchi <jiro@cpan.org>
2015-09-29 20:42:12 +0900

    Fix #2: Exit 0 when --version is given

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -12,7 +13,18 @@ var VERSION = "0.2.0-dev"
 
 func main() {
 	c := cli.NewCLI("consulkv", VERSION)
-	c.Args = os.Args[1:]
+	args := os.Args[1:]
+	for _, arg := range args {
+		if arg == "--" {
+			break
+		}
+		if arg == "-v" || arg == "--version" {
+			fmt.Println(VERSION)
+			os.Exit(0)
+		}
+	}
+
+	c.Args = args
 	c.Commands = Commands
 
 	exitStatus, err := c.Run()
